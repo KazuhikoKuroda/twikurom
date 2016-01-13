d49c4820d776dc995d6f9651aba021e67953c916
KazuhikoKuroda <kuroda2395@gmail.com>
2016-01-13 16:17:06 +0900

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twikuromApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the twikuromApp
+ */
+angular.module('twikuromApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twikuromApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the twikuromApp
+ */
+angular.module('twikuromApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
