11b4d3c135055ef97722b690addff2f74ab9cfba
KazuhikoKuroda <kuroda2395@gmail.com>
2016-01-15 11:44:49 +0900

    adapt angular style

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,62 +1,57 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name twikuromApp.controller:TwitterController
- * @description
- * # TwitterController
- * Controller of the twikuromApp
- */
-angular.module('twikuromApp')
-	.controller('TwitterController', function($scope, $q, twitterService) {
-		$scope.tweets = [];
-		$scope.rateLimitError = false;
+function TwitterController($scope, $q, twitterService) {
+	$scope.tweets = [];
+	$scope.rateLimitError = false;
 
-		twitterService.initialize();
+	twitterService.initialize();
 
-		$scope.refreshTimeline = function(maxId) {
-			twitterService.getLatestTweets(maxId).then(function(result) {
-				$scope.tweets = $scope.tweets.concat(result);
-			}, function() {
-				$scope.rateLimitError = true;
-			});
-		};
+	$scope.refreshTimeline = function(maxId) {
+		twitterService.getLatestTweets(maxId).then(function(result) {
+			$scope.tweets = $scope.tweets.concat(result);
+		}, function() {
+			$scope.rateLimitError = true;
+		});
+	};
 
-		$scope.connect = function() {
-			twitterService.connectTwitter().then(function() {
-				if (twitterService.isReady()) {
-					$('#connectButton').fadeOut(function() {
-						$('#getTimelineButton, #signOut').fadeIn();
-						$scope.refreshTimeline();
-						$scope.connectedTwitter = true;
-					});
-				} else {
-					console.log('unable to connect twitter');
-				}
-			});
-		};
-
-		$scope.signOut = function() {
-			twitterService.clearCache();
-			$scope.tweets.length = 0;
-			$('#getTimelineButton, #signOut').fadeOut(function() {
-				$('#connectButton').fadeIn();
-			});
-		};
-
-		$scope.getMe = function() {
-			twitterService.getMe()
-				.then(function(result) {
-					console.log(result);
-				}, function() {
-					console.error('error');
+	$scope.connect = function() {
+		twitterService.connectTwitter().then(function() {
+			if (twitterService.isReady()) {
+				$('#connectButton').fadeOut(function() {
+					$('#getTimelineButton, #signOut').fadeIn();
+					$scope.refreshTimeline();
+					$scope.connectedTwitter = true;
 				});
-		};
+			} else {
+				console.log('unable to connect twitter');
+			}
+		});
+	};
 
-		if (twitterService.isReady()) {
-			$('#connectButton').hide();
-			$('#getTimelineButton, #signOut').show();
-			$scope.connectedTwitter = true;
-			$scope.refreshTimeline();
-		}
-	});
+	$scope.signOut = function() {
+		twitterService.clearCache();
+		$scope.tweets.length = 0;
+		$('#getTimelineButton, #signOut').fadeOut(function() {
+			$('#connectButton').fadeIn();
+		});
+	};
+
+	$scope.getMe = function() {
+		twitterService.getMe()
+			.then(function(result) {
+				console.log(result);
+			}, function() {
+				console.error('error');
+			});
+	};
+
+	if (twitterService.isReady()) {
+		$('#connectButton').hide();
+		$('#getTimelineButton, #signOut').show();
+		$scope.connectedTwitter = true;
+		$scope.refreshTimeline();
+	}
+}
+
+angular.module('twikuromApp')
+	.controller('TwitterController', TwitterController);
